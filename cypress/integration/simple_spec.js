243ebf07ea4aa1b92983f74e56ef892b37fda2d4
Valentin Hervieu <valentin@hervi.eu>
2018-06-24 19:06:09 +0200

    Setup CI

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -1,7 +1,7 @@
 describe('Drag assertions', function() {
   beforeEach(function() {
     cy.viewport(1000, 600)
-    cy.visit('http://localhost:3001')
+    cy.visit('/')
   })
   it('Display the image and cropper with correct dimension', function() {
     cy.get('img').should('have.css', 'width', '1000px')
@@ -17,16 +17,16 @@ describe('Drag assertions', function() {
 
   it('Limit the left drag if too far', function() {
     cy.get('[data-testid=container]').dragAndDrop({ x: -1000, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, -150,667, 0)')
+    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, -150.667, 0)')
   })
 
   it('Limit the right drag if too far', function() {
     cy.get('[data-testid=container]').dragAndDrop({ x: 1000, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 150,667, 0)')
+    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 150.667, 0)')
   })
 
   it('Mouse wheel should zoom', function() {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1,5, 0, 0, 1,5, 0, 0)')
+    cy.get('img').should('have.css', 'transform', 'matrix(1.5, 0, 0, 1.5, 0, 0)')
   })
 })
