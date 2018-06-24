e98341d8e3a7ddd4d14c98abfe8fa68e0a179f76
Valentin Hervieu <valentin@hervi.eu>
2018-06-24 18:37:33 +0200

    Add fix for rendering issues in Chrome

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,8 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') {
+      args.push('--disable-gpu-rasterization') // fixes rendering issues on my computer (https://github.com/cypress-io/cypress/issues/1617)
+      return args
+    }
+  })
 }
