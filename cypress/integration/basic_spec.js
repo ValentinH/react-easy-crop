64acc1a9e8ea034be6389ce0aea12b1e7ec21e57
Valentin Hervieu <valentin@hervi.eu>
2018-06-25 10:29:05 +0200

    Add more test cases

--- cypress/integration/basic_spec.js
+++ cypress/integration/basic_spec.js
@@ -3,10 +3,27 @@ describe('Basic assertions', function() {
     cy.viewport(1000, 600)
     cy.visit('/')
   })
+
   it('Display the image and cropper with correct dimension', function() {
     cy.get('img').should('have.css', 'width', '1000px')
     cy.get('img').should('have.css', 'height', '524px')
     cy.get('[data-testid=cropper]').should('have.css', 'width', '699px') // 4/3 the height of the image
     cy.get('[data-testid=cropper]').should('have.css', 'height', '524px') // height of the image
+  })
+
+  it('Display tall images and set the image and cropper with correct dimension', function() {
+    cy.visit('/?img=/images/cat.jpg')
+    cy.get('img').should('have.css', 'width', '338px')
+    cy.get('img').should('have.css', 'height', '600px')
+    cy.get('[data-testid=cropper]').should('have.css', 'width', '338px') // width of the image
+    cy.get('[data-testid=cropper]').should('have.css', 'height', '254px') // 3/4 of height of the image
+  })
+
+  it('Display the image and cropper with correct dimension after window resize', function() {
+    cy.viewport(600, 1000)
+    cy.get('img').should('have.css', 'width', '600px')
+    cy.get('img').should('have.css', 'height', '314px')
+    cy.get('[data-testid=cropper]').should('have.css', 'width', '419px') // 4/3 the height of the image
+    cy.get('[data-testid=cropper]').should('have.css', 'height', '314px') // height of the image
   })
 })
--- examples/src/index.js
+++ examples/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import queryString from 'query-string'
 import Cropper from '../../src'
 import './styles.css'
 
+const urlArgs = queryString.parse(window.location.search)
+const imageSrc = urlArgs.img || '/images/dog.jpeg' // so we can change the image from our tests
+
 class App extends React.Component {
   state = {
-    imageSrc: '/images/dog.jpeg',
+    imageSrc,
     crop: { x: 0, y: 0 },
     zoom: 1,
     aspect: 4 / 3,
