c60394b72f299256193bad9571c1a69569896af8
Juntae Kim <bexoss@gmail.com>
2018-10-23 18:12:50 +0900

    add onError prop to Img
    add onError prop to Img

--- src/index.js
+++ src/index.js
@@ -261,6 +261,7 @@ class Cropper extends React.Component {
           src={this.props.image}
           innerRef={el => (this.image = el)}
           onLoad={this.onImgLoad}
+          onError={this.onImgError}
           alt=""
           style={{
             transform: `translate(${x}px, ${y}px) scale(${zoom})`,
