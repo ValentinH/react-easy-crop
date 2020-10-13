4e49d4189b2b5c5d28acc1d26fcf706c54244ff1
Darren Labithiotis <labithiotis@users.noreply.github.com>
2020-10-13 09:14:36 +0200

    Export props to allow other libraries to reference it (#181)
    * Export props to allow other libraries to ref it
    
    * Rename to be consistent with component

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ import {
 } from './helpers'
 import cssStyles from './styles.css'
 
-type Props = {
+export type CropperProps = {
   image?: string
   video?: string
   transform?: string
@@ -59,7 +59,7 @@ type State = {
 const MIN_ZOOM = 1
 const MAX_ZOOM = 3
 
-class Cropper extends React.Component<Props, State> {
+class Cropper extends React.Component<CropperProps, State> {
   static defaultProps = {
     zoom: 1,
     rotation: 0,
@@ -132,7 +132,7 @@ class Cropper extends React.Component<Props, State> {
     this.props.zoomWithScroll && this.clearScrollEvent()
   }
 
-  componentDidUpdate(prevProps: Props) {
+  componentDidUpdate(prevProps: CropperProps) {
     if (prevProps.rotation !== this.props.rotation) {
       this.computeSizes()
       this.recomputeCropPosition()
