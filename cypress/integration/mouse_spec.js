50f5b160933c945224d9150846f661b7c17d8205
Valentin Hervieu <valentin@hervi.eu>
2021-12-11 22:42:46 +0100

    use percy for all specs

--- cypress/integration/mouse_spec.js
+++ cypress/integration/mouse_spec.js
@@ -6,61 +6,61 @@ describe('Mouse assertions', function () {
 
   it('Move the image with mouse', function () {
     cy.get('[data-testid=container]').dragAndDrop({ x: 50, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 50, 0)')
+    cy.percySnapshot()
   })
 
   it('Limit the left drag if too far', function () {
     cy.get('[data-testid=container]').dragAndDrop({ x: -1000, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, -150.96, 0)')
+    cy.percySnapshot()
   })
 
   it('Limit the right drag if too far', function () {
     cy.get('[data-testid=container]').dragAndDrop({ x: 1000, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 150.96, 0)')
+    cy.percySnapshot()
   })
 
   it('Mouse wheel should zoom in and out', function () {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100, clientX: 500, clientY: 300 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.5, 0, 0, 1.5, 0, 0)')
+    cy.percySnapshot()
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: 50, clientX: 500, clientY: 300 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.25, 0, 0, 1.25, 0, 0)')
+    cy.percySnapshot()
   })
 
   it('Mouse wheel should zoom in and out following the pointer', function () {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100, clientX: 0, clientY: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.5, 0, 0, 1.5, 250, 130.89)')
+    cy.percySnapshot()
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: 50, clientX: 800, clientY: 400 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.25, 0, 0, 1.25, 258.333, 65.445)')
+    cy.percySnapshot()
   })
 
   it('Move down and right after zoom', function () {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100, clientX: 500, clientY: 300 })
     cy.get('[data-testid=container]').dragAndDrop({ x: 50, y: 50 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.5, 0, 0, 1.5, 50, 50)')
+    cy.percySnapshot()
   })
 
   it('Move up and left after zoom', function () {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100, clientX: 500, clientY: 300 })
     cy.get('[data-testid=container]').dragAndDrop({ x: -50, y: -50 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.5, 0, 0, 1.5, -50, -50)')
+    cy.percySnapshot()
   })
 
   it('Limit top after zoom ', function () {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100, clientX: 500, clientY: 300 })
     cy.get('[data-testid=container]').dragAndDrop({ x: 0, y: -1000 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.5, 0, 0, 1.5, 0, -130.89)')
+    cy.percySnapshot()
   })
 
   it('Limit bottom after zoom ', function () {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100, clientX: 500, clientY: 300 })
     cy.get('[data-testid=container]').dragAndDrop({ x: 0, y: 1000 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.5, 0, 0, 1.5, 0, 130.89)')
+    cy.percySnapshot()
   })
 
   it('Keep image under crop area after zoom out', function () {
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: -100, clientX: 500, clientY: 300 }) // zoom-in
     cy.get('[data-testid=container]').dragAndDrop({ x: 0, y: 1000 }) // move image to bottom
     cy.get('[data-testid=container]').trigger('wheel', { deltaY: 100, clientX: 500, clientY: 300 }) // zoom-out
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 0, 0)')
+    cy.percySnapshot()
   })
 })
--- cypress/integration/touch_spec.js
+++ cypress/integration/touch_spec.js
@@ -6,24 +6,24 @@ describe('Touch assertions', function () {
 
   it('Move the image with touch', function () {
     cy.get('[data-testid=container]').dragAndDropWithTouch({ x: 50, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 50, 0)')
+    cy.percySnapshot()
   })
 
   it('Limit the left drag if too far', function () {
     cy.get('[data-testid=container]').dragAndDropWithTouch({ x: -1000, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, -150.96, 0)')
+    cy.percySnapshot()
   })
 
   it('Limit the right drag if too far', function () {
     cy.get('[data-testid=container]').dragAndDropWithTouch({ x: 1000, y: 0 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 150.96, 0)')
+    cy.percySnapshot()
   })
 
   it('Zoom in and out with pinch', function () {
     cy.get('[data-testid=container]').pinch({ distance: 10 })
-    cy.get('img').should('have.css', 'transform', 'matrix(2, 0, 0, 2, 500, 261.78)')
+    cy.percySnapshot()
     cy.get('[data-testid=container]').pinch({ distance: -4 })
-    cy.get('img').should('have.css', 'transform', 'matrix(1.2, 0, 0, 1.2, 100, 37.0681)')
+    cy.percySnapshot()
   })
 
   it('Zoom in and out with pinch based on the center between 2 fingers', function () {
@@ -41,7 +41,7 @@ describe('Touch assertions', function () {
         ],
       })
       .trigger('touchend')
-    cy.get('img').should('have.css', 'transform', 'matrix(1.1, 0, 0, 1.1, 0, 4.5)')
+    cy.percySnapshot()
     cy.get('[data-testid=container]')
       .trigger('touchstart', {
         touches: [
@@ -56,7 +56,7 @@ describe('Touch assertions', function () {
         ],
       })
       .trigger('touchend')
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, -36.8182, 0)')
+    cy.percySnapshot()
   })
 
   it('Move image with pinch based on the center between 2 fingers', function () {
@@ -74,17 +74,13 @@ describe('Touch assertions', function () {
         ],
       })
       .trigger('touchend')
-    cy.get('img').should('have.css', 'transform', 'matrix(1, 0, 0, 1, 100, 0)')
+    cy.percySnapshot()
   })
 
   it('Rotate with pinch', function () {
     cy.get('[data-testid=container]').rotate({ rotation: 30 })
-    cy.get('img').should('have.css', 'transform', 'matrix(0.866025, 0.5, -0.5, 0.866025, -2.5, 0)')
+    cy.percySnapshot()
     cy.get('[data-testid=container]').rotate({ rotation: -45 })
-    cy.get('img').should(
-      'have.css',
-      'transform',
-      'matrix(0.965926, -0.258819, 0.258819, 0.965926, 1.03553, -1.46447)'
-    )
+    cy.percySnapshot()
   })
 })
