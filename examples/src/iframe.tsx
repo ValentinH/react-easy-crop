95df797c8b131781f11d5619d0430a4f01bbcab6
Glen Davies <glen.davies@automattic.com>
2022-09-23 10:49:57 +1200

    Fix typing of iframe example component

--- examples/src/iframe.tsx
+++ examples/src/iframe.tsx
@@ -1,18 +1,21 @@
-// @ts-nocheck
 import * as React from 'react'
 
 import './styles.css'
 import { createPortal } from 'react-dom'
 
-export default function Iframe({ children, ...props }) {
+interface Props {
+  children: React.ReactNode
+}
+
+export default function Iframe({ children }: Props) {
   const [iframeBody, setIframeBody] = React.useState<HTMLElement>()
 
-  const iFrameRef = React.useRef<HTMLIFrameElement>()
+  const iFrameRef = React.useRef<HTMLIFrameElement>(null)
 
   React.useEffect(() => {
     function setDocumentIfReady() {
-      const { contentDocument } = iFrameRef.current
-      const { readyState, documentElement } = contentDocument
+      const { contentDocument } = iFrameRef.current as HTMLIFrameElement
+      const { readyState, documentElement } = contentDocument as Document
 
       if (readyState !== 'interactive' && readyState !== 'complete') {
         return false
@@ -24,19 +27,20 @@ export default function Iframe({ children, ...props }) {
     }
 
     // Document set with srcDoc is not immediately ready.
-    iFrameRef.current.addEventListener('load', setDocumentIfReady)
+    if (iFrameRef.current) {
+      iFrameRef.current.addEventListener('load', setDocumentIfReady)
+    }
   }, [iFrameRef])
 
   return (
     <>
       <iframe
         style={{ height: '100vh', width: '100vw' }}
-        {...props}
         ref={iFrameRef}
         srcDoc="<!doctype html>"
         title="test iframed"
       >
-        {iframeBody && createPortal(children, iframeBody)}
+        <>{iframeBody && createPortal(children, iframeBody)}</>
       </iframe>
     </>
   )
