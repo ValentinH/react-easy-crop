1aad632c8ecae17844d1cc261c87e74811d03837
Valentin Hervieu <valentin@hervi.eu>
2024-04-04 16:41:47 +0200

    please send help

--- scripts/copy-build-files.js
+++ scripts/copy-build-files.js
@@ -22,7 +22,7 @@ async function createPackageFile() {
     exports: {
       '.': {
         import: {
-          types: './index.d.mts',
+          types: './index.module.d.mts',
           default: './index.module.mjs',
         },
         require: {
